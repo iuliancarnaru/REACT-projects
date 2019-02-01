aa679d6e5016ff6bd95227abe474f56d14ed8264
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-01 19:50:42 +0000

    learning about .env.local

--- survey/src/Survey.js
+++ survey/src/Survey.js
@@ -1,119 +1,116 @@
 import React, { Component } from 'react'
-import firebase from 'firebase/app';
 import uuid from 'uuid';
 
-var config = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId
+const firebase = require('firebase/app');
+require("firebase/database");
+
+
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
+
 
 firebase.initializeApp(config);
 
-
 class Survey extends Component {
-    constructor(props) {
+    nameSubmit(event){
+        var studentName = this.refs.name.value;
+        this.setState({studentName: studentName});
+      }
+      answerSelected(event){
+        var answers = this.state.answers;
+        if(event.target.name === 'answer1'){
+          answers.answer1 = event.target.value;
+        } else if(event.target.name === 'answer2'){
+          answers.answer2 = event.target.value;
+        } else if(event.target.name === 'answer3'){
+          answers.answer3 = event.target.value;
+        }
+    
+        this.setState({answers: answers});
+      }
+    
+      questionSubmit(){
+        firebase.database().ref(this.state.uid).set({
+          studentName: this.state.studentName,
+          answers: this.state.answers
+        });
+        this.setState({isSubmitted: true});
+      }
+    
+      constructor(props){
         super(props);
     
         this.state = {
-          id: uuid.v1(),
+          uid: uuid.v1(),
           studentName: '',
           answers: {
-            ans1: '',
-            ans2: '',
-            ans3: ''
+            answer1: '',
+            answer2: '',
+            answer3: ''
           },
-          isSubmited: false
-        }
+          isSubmitted: false
+        };
+        this.nameSubmit = this.nameSubmit.bind(this);
+        this.answerSelected = this.answerSelected.bind(this);
+        this.questionSubmit = this.questionSubmit.bind(this);
       }
-
-      nameSubmit = (event) => {
-        var studentName = this.refs.name.value;
-        this.setState({
-            studentName
-        }, () => {
-            console.log(this.state)
-        })
-      }
-
-      asnwerSelected = (event) => {
-        var answers = this.state.answers;
-        if(event.target.name === 'ans1') {
-            answers.ans1 = event.target.value
-        } else if(event.target.name === 'ans2') {
-            answers.ans2 = event.target.value
-        } else if(event.target.name === 'ans3') {
-            answers.ans3 = event.target.value
-        }
-
-        this.setState({
-            answers
-        }, () => {
-            console.log(this.state)
-        })
-      }
-
-      questionsSubmit = () => {
-
-      }
-
-
-    render() {
-
-    var studentName;
-    var questions;
-
-    if (this.state.studentName === '' && this.state.isSubmited === false) {
-        studentName = <div>
-            <h1>Hey student please let us your name</h1>
+    
+      render(){
+        var studentName;
+        var questions;
+    
+        if(this.state.studentName === '' && this.state.isSubmitted === false){
+          studentName = <div>
+            <h1>Hey, please let us know your name: </h1>
             <form onSubmit={this.nameSubmit}>
-                <input className="name-input" type="text" placeholder="Enter your name" ref="name"/>
+              <input className="name-input" type="text" placeholder="Enter your name" ref="name" />
             </form>
-        </div>;
-        questions = null;
-    } else if (this.state.studentName !== '' && this.state.isSubmited === false) {
-        studentName = <h1>Welcome to our survey, {this.state.studentName}!</h1>
-        questions = <div>
-            <h2>Here are some questions</h2>
-            <form onSubmit={this.questionsSubmit}>
+          </div>;
+          questions = ''
+        } else if (this.state.studentName !== '' && this.state.isSubmitted === false){
+          studentName = <h1>Welcome to our survey page, {this.state.studentName}</h1>;
+            questions = <div>
+              <h2>Here are some questions: </h2>
+              <form onSubmit={this.questionSubmit}>
                 <div className="card">
-                    <label>What kind of courses you like the most: </label><br />
-                    <input type="radio" name="ans1" value="Technology" onChange={this.asnwerSelected}/>Technology
-                    <input type="radio" name="ans1" value="Design" onChange={this.asnwerSelected}/>Design
-                    <input type="radio" name="ans1" value="Marketing" onChange={this.asnwerSelected}/>Marketing
+                  <label>What kind of courses you like the most: </label> <br />
+                  <input type="radio" name="answer1" value="Technology" onChange={this.answerSelected} />Technology
+                  <input type="radio" name="answer1" value="Design" onChange={this.answerSelected} />Design
+                  <input type="radio" name="answer1" value="Marketing" onChange={this.answerSelected} />Marketing
                 </div>
                 <div className="card">
-                    <label>You are a: </label><br />
-                    <input type="radio" name="ans2" value="Student" onChange={this.asnwerSelected}/>Student
-                    <input type="radio" name="ans2" value="Employed" onChange={this.asnwerSelected}/>Employed
-                    <input type="radio" name="ans2" value="Freelancer" onChange={this.asnwerSelected}/>Freelancer
+                  <label>you are a: </label> <br />
+                  <input type="radio" name="answer2" value="Student" onChange={this.answerSelected} />Student
+                  <input type="radio" name="answer2" value="in-job" onChange={this.answerSelected} />in-job
+                  <input type="radio" name="answer2" value="looking-job" onChange={this.answerSelected} />looking-job
                 </div>
                 <div className="card">
-                    <label>Is online learning helpful: </label><br />
-                    <input type="radio" name="ans3" value="yes" onChange={this.asnwerSelected}/>Yes
-                    <input type="radio" name="ans3" value="no" onChange={this.asnwerSelected}/>No
-                    <input type="radio" name="ans3" value="more-time" onChange={this.asnwerSelected}/>I need more time
+                  <label>Is online learning helpful:  </label> <br />
+                  <input type="radio" name="answer3" value="yes" onChange={this.answerSelected} />yes
+                  <input type="radio" name="answer3" value="no" onChange={this.answerSelected} />no
+                  <input type="radio" name="answer3" value="maybe" onChange={this.answerSelected} />maybe
                 </div>
-
-                <input className="feedback-button" type="submit" value="submit"/>
-            </form>
-        </div>
-    } else if (this.state.isSubmited === true && this.state.studentName) {
-        studentName = <h1>
-            Thanks, {this.state.studentName}! Have a nice day!
-        </h1>
-    }
-
-    return (
-      <div>
-        {studentName}
-        {questions}
-      </div>
-    )
-  }
+                <input className="feedback-button" type="submit" value="submit" />
+              </form>
+            </div>
+        } else if(this.state.isSubmitted === true){
+          studentName = <h1>Thanks, {this.state.studentName}</h1>
+        }
+    
+        return(
+          <div>
+            {studentName}
+            --------------------------------
+            {questions}
+          </div>
+        );
+      }
 }
 
 export default Survey;
