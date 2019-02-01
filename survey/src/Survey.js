a93cee8467682f17d9bc934d8de7d9ab168cd768
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-01 16:20:30 +0000

    survey page test

---
+++ survey/src/Survey.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Survey extends Component {
+  render() {
+    return (
+      <div>
+        I am from survey component
+      </div>
+    )
+  }
+}
+
+export default Survey;
+
